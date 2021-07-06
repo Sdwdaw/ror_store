6e8f1aedf46a9ecb708a4cbed61f29004b7aaea1
sdwdaw <sergiyzhbanov@gmail.com>
2021-07-06 15:56:08 +0300

    3 add new columns & rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_06_104603) do
+ActiveRecord::Schema.define(version: 2021_07_06_121412) do
 
   create_table "items", force: :cascade do |t|
     t.float "price"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2021_07_06_104603) do
     t.float "weight"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "description"
   end
 
 end
